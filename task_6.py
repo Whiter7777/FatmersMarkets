9796e0cc1722ead0a874aede8f920b16c27c8d67
Whiter7777 <bel_pavel@mail.ru>
2022-12-06 21:50:22 +0300

    Add files

---
+++ task_6.py
@@ -0,0 +1,13 @@
+d = {}
+
+with open("Export.csv", "r") as inf:
+    for line in inf:
+        line = line.strip().split(",")
+        d[line[0]] = line[1:]
+
+city = "Danville"
+
+for val in d.values():
+    if val[7] == city:
+        print(val[0], val[7], val[9], val[19], val[20])
+
